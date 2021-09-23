f776d62c7bb48cbb4465b55e38187d851f1261a9
Meenakshi1215 <91046694+Meenakshi1215@users.noreply.github.com>
2021-09-23 09:51:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+(function() {
+    var app = angular.module('customersApp', ['ngRoute']);
+    app.config(function($routeProvider){
+        $routeProvider
+             .when('/',{
+            controller:'CustomersController',
+            templateUrl:'app/views/customers.html',
+        })
+        .when('/orders/:customerId',{
+            controller:'OrderssController',
+            templateUrl:'app/views/orders.html',
+        })
+        .otherwise({ redirectTo: '/' });
+    });
+}());
